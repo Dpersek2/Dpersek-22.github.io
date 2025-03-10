635a8fb7cf50e45faa99b52bbd5408f5a4f3b52a
Dpersek2 <dpersek@highpoint.edu>
2025-03-09 22:41:39 -0400

    dark hotfix

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -7,43 +8,41 @@ import Work from "./components/Work";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-import { useState, useEffect } from "react";
-
-
 export default function Home() {
+  // Default to light mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-const [isDarkMode, setIsDarkMode] = useState(false)
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
 
-useEffect(()=>{
-  if(
-    localStorage.theme === 'dark' || 
-    (!('theme' in localStorage) && 
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ){
-    setIsDarkMode(true);
-}else{
-  setIsDarkMode(false);
-}
-},[])
-
-useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }else{
-      document.documentElement.classList.remove('dark');
+    if (savedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
     }
-},[isDarkMode])
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light"); // Ensure light mode is explicitly stored
+    }
+  }, [isDarkMode]);
 
   return (
     <>
-    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <About  isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Skills isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Work isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Contact isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Footer isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <About isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Skills isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Work isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Contact isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Footer isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
     </>
   );
 }
